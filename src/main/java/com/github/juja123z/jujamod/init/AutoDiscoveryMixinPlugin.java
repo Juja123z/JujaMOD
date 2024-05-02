1b4fb15bc174a920cf35a2347e32465d9c885d6c
GitHub Action <action@github.com>
2024-05-02 04:56:24 +0000

    Initialize template repository proper
    Set package name to com.github.juja123z.jujamod
    Set project name to JujaMOD
    Set modid to jujamod

--- src/main/java/com/github/juja123z/jujamod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/juja123z/jujamod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.juja123z.jujamod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
--- src/main/java/com/github/juja123z/jujamod/mixin/MixinGuiMainMenu.java
+++ src/main/java/com/github/juja123z/jujamod/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.juja123z.jujamod.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
